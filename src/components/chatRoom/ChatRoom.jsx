8d7f07fa934b19d3e7d50c359d87178dd8b412c8
NilsFaelt <92431259+NilsFaelt@users.noreply.github.com>
2022-06-13 21:36:47 +0200

    all css done

--- src/components/chatRoom/ChatRoom.jsx
+++ src/components/chatRoom/ChatRoom.jsx
@@ -1,7 +1,25 @@
 import Styles from "./chatRoom.module.css";
-
+const room = "The Room";
 const ChatRoom = () => {
-  return <div className={Styles.container}></div>;
+  return (
+    <div className={Styles.container}>
+      <h3 className={Styles.title}>{room}</h3>
+      <div className={Styles.messaegAndUserContainer}>
+        <div className={Styles.messeageContainer}>
+          <div className={Styles.displayMessage}></div>
+          <input
+            placeholder='Write messeage....'
+            className={Styles.input}
+            type='text'
+          />
+        </div>
+        <section className={Styles.displayUsers}>
+          <h4 className={Styles.userTitle}>Active users</h4>
+        </section>
+      </div>
+      <button>Send</button>
+    </div>
+  );
 };
 
 export default ChatRoom;
