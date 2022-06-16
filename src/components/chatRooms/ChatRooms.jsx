56663f83d3ca5a907d556c020678b916e8512e0e
NilsFaelt <92431259+NilsFaelt@users.noreply.github.com>
2022-06-16 17:29:48 +0200

    rooms liogic

--- src/components/chatRooms/ChatRooms.jsx
+++ src/components/chatRooms/ChatRooms.jsx
@@ -1,17 +1,39 @@
+import { useState } from "react";
 import ChatRoom from "../chatRoom/ChatRoom";
 import Styles from "./chatRooms.module.css";
 
 const ChatRooms = () => {
+  const [rooms, setRoooms] = useState([]);
+  console.log(rooms);
+
+  const addRoom = () => {
+    const room = prompt("Add roomname");
+    if (room.length > 12) {
+      alert("max length is 12 charachters, please try again");
+    } else {
+      setRoooms([...rooms, room]);
+    }
+  };
+
   return (
     <main className={Styles.container}>
       <ChatRoom />
       <section className={Styles.chatRoomContainer}>
         <h3 className={Styles.roomsTitle}>Rooms</h3>
-        <div className={Styles.rooms}></div>
+        <div className={Styles.rooms}>
+          {rooms.length > 0 ? (
+            rooms.map((room) => {
+              return <h4 className={Styles.roomTitle}>{room}</h4>;
+            })
+          ) : (
+            <p className={Styles.userTitle}> No active rooms at the moment</p>
+          )}
+        </div>
         <button
           style={{
             width: "10vw",
           }}
+          onClick={addRoom}
         >
           Add new room
         </button>
